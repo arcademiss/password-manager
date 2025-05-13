5552a0ae3a72250fe56ad595b15ca64fc727081e
arcademiss <georgeventel97@gmail.com>
2025-05-13 19:51:10 +0300

    encryption of user passwords done

--- src/server/main.py
+++ src/server/main.py
@@ -4,7 +4,8 @@ from fastapi import FastAPI, HTTPException
 from dotenv import load_dotenv
 import os
 from hashlib import pbkdf2_hmac as pbkdf2
-
+from Crypto.Cipher import AES
+import uvicorn
 import uuid
 
 from models import UserRegistration, UserLogin
@@ -25,6 +26,16 @@ app = FastAPI()
 def hash_password(password: str, salt: bytes) -> str:
     return pbkdf2('sha256', password.encode('utf-8'), salt, 600_000, dklen=32).hex()
 
+def encrypt_password(password):
+    cipher = AES.new(bytes.fromhex(SERVER_SECRET), AES.MODE_EAX)
+    nonce = cipher.nonce
+    ciphertext, tag = cipher.encrypt_and_digest(bytes.fromhex(password))
+
+    return ciphertext, nonce, tag
+
+def decrypt_password(password, nonce):
+    cipher = AES.new(bytes.fromhex(SERVER_SECRET), AES.MODE_EAX, nonce=nonce)
+    return cipher.decrypt(bytes.fromhex(password))
 
 @app.post("/register")
 async def register_user(user: UserRegistration):
@@ -38,12 +49,16 @@ async def register_user(user: UserRegistration):
             # Generate salt and hash
             salt = os.urandom(16)  # 16-byte salt
             hashed_password = hash_password(user.password, salt)
+            # todo: encrypt hashed password
+
+            encrypted_password, nonce, tag = encrypt_password(hashed_password)
 
             # Insert into DB
             user_uuid = uuid.uuid4()
             curr.execute(
-                "INSERT INTO users (uuid, username, hashed_password, salt) VALUES (%s, %s, %s, %s)",
-                (user_uuid, user.username, hashed_password, salt.hex())
+                "INSERT INTO users (uuid, username, hashed_password, salt, encryption_nonce, encryption_tag) "
+                "VALUES (%s, %s, %s, %s, %s, %s)",
+                (user_uuid, user.username, encrypted_password.hex(), salt.hex(), nonce, tag)
             )
             conn.commit()
             return {"status": "User created"}
@@ -54,19 +69,24 @@ async def login_user(user: UserLogin):
     with psycopg.connect(DB_CONNECT_STR) as conn:
         with conn.cursor() as curr:
             curr.execute("""
-                SELECT uuid, hashed_password, salt FROM users WHERE username = %s
+                SELECT uuid, hashed_password, salt, encryption_nonce, encryption_tag FROM users WHERE username = %s
             """, (user.username,))
             result = curr.fetchone()
 
             if not result:
                 raise HTTPException(status_code=401, detail="Invalid Credentials")
 
-            user_uuid, hashed_password, salt_hex = result
+            user_uuid, hashed_password, salt_hex, nonce, encryption_tag = result
             salt = bytes.fromhex(salt_hex)  # Correctly decode hex string to bytes
 
             user_pass_hash = hash_password(user.password, salt)
-            if user_pass_hash == hashed_password:
+
+            decrypted_password = decrypt_password(hashed_password, nonce).hex()
+
+            if user_pass_hash == decrypted_password:
                 return {"status": "success"}
             else:
                 raise HTTPException(status_code=401, detail="Invalid Credentials")
+
+
 
